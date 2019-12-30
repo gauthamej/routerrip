b0784e1b960bbfcd24a41e8aa96098034a78097a
gauthamej <52912918+gauthamej@users.noreply.github.com>
2019-12-30 22:08:56 +0530

    Update Mainserver.js

--- src/Mainserver.js
+++ src/Mainserver.js
@@ -139,6 +139,15 @@ var sql=  "UPDATE free SET name='"+edname+"', projectname='"+edprojectname+"', n
         })
        })
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  // Handle React routing, return all requests to React app
+  app.get('*', (request, response) => {
+    response.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+  })
+}
+
        
 
       	
